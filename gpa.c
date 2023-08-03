389ccb1502319b0c7b0714703156d3a9f51e95fc
Joint55internal <108395345+Joint55internal@users.noreply.github.com>
2023-08-04 02:34:15 +0900

    Update gpa.c
    Display an  error message when invalid inputs are made

--- gpa.c
+++ gpa.c
@@ -28,29 +28,67 @@ void gpa_calc(void) {
     printf("Hello, %s\n", studentName);
     printf("Enter Student ID: ");
     scanf("%d", &studentID);
-    //number of total courses that student took =
-    printf("Total Number of course : ");
-    scanf("%d", &totalCourses);
-    //print number of grades that are require to calculate GPA
-    printf("Number of A+s: ");
-    scanf(" %d", &sScoreAA); 
-    printf("Number of As: ");
-    scanf(" %d", &sScoreA); 
-    printf("Number of Bs: ");
-    scanf(" %d", &sScoreB); 
-    printf("Number of Cs: ");
-    scanf(" %d", &sScoreC); 
+    
+    // total number of courses
+    do {
+        printf("Total Number of courses: ");
+        if (scanf("%d", &totalCourses) != 1 || totalCourses < 0) {
+            printf("Error: Invalid input. Enter a non-negative number of courses.\n");
+            // Clear the input buffer in case of invalid input
+            while (getchar() != '\n');
+        }
+    } while (totalCourses < 0);
+
+    //  A+ grades
+    do {
+        printf("Number of A+s: ");
+        if (scanf("%d", &sScoreAA) != 1 || sScoreAA < 0) {
+            printf("Error: Invalid input. Enter a non-negative number of A+ grades.\n");
+            // Clear the input buffer in case of invalid input
+            while (getchar() != '\n');
+        }
+    } while (sScoreAA < 0);
+
+    // A grades
+    do {
+        printf("Number of As: ");
+        if (scanf("%d", &sScoreA) != 1 || sScoreA < 0) {
+            printf("Error: Invalid input. Enter a non-negative number of As.\n");
+            // Clear the input buffer in case of invalid input
+            while (getchar() != '\n');
+        }
+    } while (sScoreA < 0);
+
+    // B grades
+    do {
+        printf("Number of Bs: ");
+        if (scanf("%d", &sScoreB) != 1 || sScoreB < 0) {
+            printf("Error: Invalid input. Enter a non-negative number of Bs.\n");
+            // Clear the input buffer in case of invalid input
+            while (getchar() != '\n');
+        }
+    } while (sScoreB < 0);
+
+    //  C grades
+    do {
+        printf("Number of Cs: ");
+        if (scanf("%d", &sScoreC) != 1 || sScoreC < 0) {
+            printf("Error: Invalid input. Enter a non-negative number of Cs.\n");
+            // Clear the input buffer in case of invalid input
+            while (getchar() != '\n');
+        }
+    } while (sScoreC < 0);
 
     // Calculate GPA with school's rules
     float totalPoints = (sScoreAA * 5) + (sScoreA * 4) + (sScoreB * 3) + (sScoreC * 2) + (sScoreF * 0);
     int B = sScoreAA + sScoreA + sScoreB + sScoreC + sScoreF;
     T = totalPoints / totalCourses;
-    C = totalCourses - B ;
-    //calculate 5 sclae GPA to 4 scale GPT
+    C = totalCourses - B;
+    // Calculate 5 scale GPA to 4 scale GPA
     T4 = (T / 5.0) * 4.0;
 
-    //print the result with information about the student
-    if (C == 0 || C > 0) {
+    // Print the result with information about the student
+     if (C == 0 || C > 0) {
         printf("=========================\n");
         printf("StudentName: %s\n", studentName);
         printf("StudentID: %d\n", studentID);
@@ -62,7 +100,8 @@ void gpa_calc(void) {
     }
     else if (C < 0) { 
         printf("=========================\n");
-        printf ("Warning!: Check it again, You probably made a mistake!\n");
+        printf ("[*] Warning!: Check it again, You probably made a mistake!\n");
+        printf ("[*] If this is below 0, Your imput might be incorrect: %d\n", C);
         printf("StudentName: %s\n", studentName);
         printf("StudentID: %d\n", studentID);
         printf("Number of course: %d\n", totalCourses);
@@ -71,7 +110,6 @@ void gpa_calc(void) {
         printf ("Your GPA(4): %.2f\n", T4);
         printf("=========================\n");
     }
-
 }
 
 int main() {
