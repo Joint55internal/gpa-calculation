f5d635e387e51f4633a9e1dac61772e472cf4bb5
Joint55internal <108395345+Joint55internal@users.noreply.github.com>
2023-08-04 01:49:39 +0900

    Add files via upload

--- gpa.c
+++ gpa.c
@@ -1,7 +1,8 @@
 // This program can be used to easily calculate your GPA
+// This can also suggest by parents to what to do according to student's grade
 // 
 /*========================================
- *        GPA calculation version 1.0
+ *        GPA calculation version 1.1
  *         
  *         (C) 2023 Toshiki
  *  
@@ -10,24 +11,18 @@
 #include <stdio.h>
 #include <string.h>
 
-int result;
-int X;
-int Y;
-int Z;
-int A;
-int B;
-int C;
-float T;
-float T4;
 void gpa_calc(void) {
     char studentName[100];
     int studentID;
-    int number;
+    int totalCourses;
     int sScoreAA = 0;
     int sScoreA = 0;
     int sScoreB = 0;
     int sScoreC = 0;
     int sScoreF = 0;
+    int C;
+    float T;
+    float T4;
 
     printf("Enter your name: ");
     scanf("%s", studentName);
@@ -36,7 +31,7 @@ void gpa_calc(void) {
     scanf("%d", &studentID);
     //number of total courses that student took =
     printf("Total Number of course : ");
-    scanf("%d", &number);
+    scanf("%d", &totalCourses);
     //print number of grades that are require to calculate GPA
     printf("Number of A+s: ");
     scanf(" %d", &sScoreAA); 
@@ -46,25 +41,23 @@ void gpa_calc(void) {
     scanf(" %d", &sScoreB); 
     printf("Number of Cs: ");
     scanf(" %d", &sScoreC); 
-    //calculate GPA with School's Rules
-    X = sScoreAA * 5;
-    Y = sScoreA * 4;
-    Z = sScoreB * 3;
-    A = sScoreC * 2;
-    T = (X + Y + Z + A)/number;
-    B = sScoreAA + sScoreA + sScoreB + sScoreC;
-    C = number - B ;
+
+    // Calculate GPA with school's rules
+    float totalPoints = (sScoreAA * 5) + (sScoreA * 4) + (sScoreB * 3) + (sScoreC * 2) + (sScoreF * 0);
+    int B = sScoreAA + sScoreA + sScoreB + sScoreC + sScoreF;
+    T = totalPoints / totalCourses;
+    C = totalCourses - B ;
     //calculate 5 sclae GPA to 4 scale GPT
-    T4 = (T / 5) * 4;
+    T4 = (T / 5.0) * 4.0;
 
     //print the result with information about the student
     printf("=========================\n");
     printf("StudentName: %s\n", studentName);
     printf("StudentID: %d\n", studentID);
-    printf("Number of course: %d\n", number);
+    printf("Number of course: %d\n", totalCourses);
     printf("Number of course You've received Credit: %d\n", B);
-    printf ("Your GPA(5): %f\n", T);
-    printf ("Your GPA(4): %f\n", T4);
+    printf ("Your GPA(5): %.2f\n", T);
+    printf ("Your GPA(4): %.2f\n", T4);
     printf ("* If this is below 0, Your imput might be incorrect: %d\n", C);
     printf("=========================\n");
 
